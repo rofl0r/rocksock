0531f1073720cdf30a438c2540baa72b7bfdcbc2
rofl0r <retnyg@gmx.net>
2013-11-12 01:17:13 +0100

    rocksock_add_proxy: add missing return statement

--- rocksock_add_proxy.c
+++ rocksock_add_proxy.c
@@ -1,6 +1,4 @@
-#ifdef _POSIX_C_SOURCE
 #undef _POSIX_C_SOURCE
-#endif
 #define _POSIX_C_SOURCE 200809L
 #undef _GNU_SOURCE
 #define _GNU_SOURCE
@@ -16,10 +14,14 @@
 
 int rocksock_add_proxy(rocksock* sock, rs_proxyType proxytype, char* host, unsigned short port, char* username, char* password) {
 	rs_proxy* prx;
-	if (!sock) return RS_E_NULL;
-	if(sock->lastproxy+1 >= MAX_PROXIES) return rocksock_seterror(sock, RS_ET_OWN, RS_E_EXCEED_PROXY_LIMIT, ROCKSOCK_FILENAME, __LINE__);
-	if(!host) return rocksock_seterror(sock, RS_ET_OWN, RS_E_NULL, ROCKSOCK_FILENAME, __LINE__);
-	if(proxytype == RS_PT_SOCKS4 && (username || password)) rocksock_seterror(sock, RS_ET_OWN, RS_E_SOCKS4_NOAUTH, ROCKSOCK_FILENAME, __LINE__);
+	if (!sock)
+		return RS_E_NULL;
+	if(sock->lastproxy+1 >= MAX_PROXIES)
+		return rocksock_seterror(sock, RS_ET_OWN, RS_E_EXCEED_PROXY_LIMIT, ROCKSOCK_FILENAME, __LINE__);
+	if(!host)
+		return rocksock_seterror(sock, RS_ET_OWN, RS_E_NULL, ROCKSOCK_FILENAME, __LINE__);
+	if(proxytype == RS_PT_SOCKS4 && (username || password))
+		return rocksock_seterror(sock, RS_ET_OWN, RS_E_SOCKS4_NOAUTH, ROCKSOCK_FILENAME, __LINE__);
 	if(proxytype == RS_PT_SOCKS5 && ((username && strlen(username) > 255) || (password && strlen(password) > 255)))
 		return rocksock_seterror(sock, RS_ET_OWN, RS_E_SOCKS5_AUTH_EXCEEDSIZE, ROCKSOCK_FILENAME, __LINE__);
 	sock->lastproxy++;
